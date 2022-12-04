29bd2a0e1a8faed3232e9e87e3858a7027337584
Daniel Marin <danielmarin998@hotmail.com>
2022-12-04 18:41:32 -0500

    started project

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae natus libero, tenetur earum quis blanditiis, ex debitis dolorum odit similique laboriosam commodi voluptate iusto doloremque labore ab alias! Non, temporibus?</p>
+          <span>Don't your have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  )
+}
+
+export default login
