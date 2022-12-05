07b81d2308edfd2b5ecb237dd1d52471648b9cf0
Daniel Marin <danielmarin998@hotmail.com>
2022-12-04 19:44:13 -0500

    Login finished

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import './login.scss'
 
-function login() {
+function Login() {
   return (
     <div className="login">
       <div className="card">
         <div className="left">
           <h1>Hello World</h1>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae natus libero, tenetur earum quis blanditiis, ex debitis dolorum odit similique laboriosam commodi voluptate iusto doloremque labore ab alias! Non, temporibus?</p>
-          <span>Don't your have an account?</span>
+          <span>Don't you have an account?</span>
           <button>Register</button>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <h1>Login</h1>
+          <form action="">
+            <input type="text" placeholder='Username' />
+            <input type="password" placeholder='Password' />
+            <button>Login</button>
+          </form>
+        </div>
       </div>
     </div>
   )
 }
 
-export default login
+export default Login
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,24 @@ import './register.scss'
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae natus libero, tenetur earum quis blanditiis, ex debitis dolorum odit similique laboriosam commodi voluptate iusto doloremque labore ab alias! Non, temporibus?</p>
+          <span>Do you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form action="">
+            <input type="text" placeholder='Username' />
+            <input type="password" placeholder='Password' />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
